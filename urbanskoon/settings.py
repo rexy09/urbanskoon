8e201278b0394bf66f50d8d12cbefc5df825172d
rexy09 <fredmuju@gmail.com>
2024-12-28 02:50:58 +0300

    deploy

--- urbanskoon/settings.py
+++ urbanskoon/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,9 +86,7 @@ WSGI_APPLICATION = 'urbanskoon.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
