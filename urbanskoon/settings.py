21296391ac6424f9719770a863d29383bdb66b96
rexy09 <fredmuju@gmail.com>
2024-12-28 02:57:01 +0300

    deploy

--- urbanskoon/settings.py
+++ urbanskoon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +127,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     (BASE_DIR / "static"),
 ]
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+# STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 # Media Files upload settings
