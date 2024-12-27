93f1273e57f4f4ae5514c0922ad917d3ccb38f64
rexy09 <fredmuju@gmail.com>
2024-12-28 02:30:26 +0300

    deploy

--- urbanskoon/urls.py
+++ urbanskoon/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
 ]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
