ab2b22d9f41b59eaa792d9c5c20c0f465d1bc2f2
rexy09 <fredmuju@gmail.com>
2024-12-28 02:05:39 +0300

    deploy

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
