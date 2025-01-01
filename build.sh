ab980e7defe3107344e1c8e037cce12a4005c138
rexy09 <fredmuju@gmail.com>
2025-01-01 15:33:22 +0300

    deploy statics

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
-echo "Make Migrations..."
+# echo "Make Migrations..."
 # python3 manage.py makemigrations --noinput
 # python3 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
+# echo "Collect Static..."
+# python3 manage.py collectstatic --noinput --clear
