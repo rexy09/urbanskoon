fdbb108415d60731ef981051432addda886b8426
rexy09 <fredmuju@gmail.com>
2025-01-02 14:55:27 +0300

    refactor settings.py to comment out database configuration

--- urbanskoon/settings.py
+++ urbanskoon/settings.py
@@ -87,18 +87,17 @@ WSGI_APPLICATION = 'urbanskoon.wsgi.application'
 #     }
 # }
 
-if os.environ["PGDATABASE"]:
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ["PGDATABASE"],
-            'USER': os.environ["PGUSER"],
-            'PASSWORD': os.environ["PGPASSWORD"],
-            'HOST': os.environ["PGHOST"],
-            'PORT': os.environ["PGPORT"],
-        }
-    }
+# if os.environ["PGDATABASE"]:  
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': os.environ["PGDATABASE"],
+#             'USER': os.environ["PGUSER"],
+#             'PASSWORD': os.environ["PGPASSWORD"],
+#             'HOST': os.environ["PGHOST"],
+#             'PORT': os.environ["PGPORT"],
+#         }
+#     }
 
 
 
