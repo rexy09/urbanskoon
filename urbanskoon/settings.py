946d1119f6edbf96df35baef87f90cd4af641502
rexy09 <fredmuju@gmail.com>
2025-01-02 17:21:16 +0300

    add SVG icons and JavaScript for admin interface; update settings and README for static files

--- urbanskoon/settings.py
+++ urbanskoon/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     (BASE_DIR / "static"),
--- urbanskoon/urls.py
+++ urbanskoon/urls.py
@@ -21,10 +21,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # urlpatterns += path("__reload__/", include("django_browser_reload.urls"))
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
 ]
 if settings.DEBUG:
-    urlpatterns += path("__reload__/", include("django_browser_reload.urls"))
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
